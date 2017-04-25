a377e7837d41cba04ba5fedfa065c31be3071560
sdelamo <sergio.delamo@softamo.com>
2017-04-25 07:50:16 +0200

    Capture exception

--- grails-googlecloud-translate/grails-app/services/org/grails/plugins/googlecloud/translate/GoogleCloudTranslateService.groovy
+++ grails-googlecloud-translate/grails-app/services/org/grails/plugins/googlecloud/translate/GoogleCloudTranslateService.groovy
@@ -1,15 +1,23 @@
 package org.grails.plugins.googlecloud.translate
 
 import com.google.cloud.translate.Translate.TranslateOption
+import com.google.cloud.translate.TranslateException
 import com.google.cloud.translate.TranslateOptions
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
+@Slf4j
 @CompileStatic
 class GoogleCloudTranslateService {
 
     String translateTextFromSourceToTarget(String text, String source, String target) {
-        TranslateOptions.defaultInstance.service.translate(text,
-                TranslateOption.sourceLanguage(source),
-                TranslateOption.targetLanguage(target)).translatedText
+        try {
+            return TranslateOptions.defaultInstance.service.translate(text,
+                    TranslateOption.sourceLanguage(source),
+                    TranslateOption.targetLanguage(target)).translatedText
+        } catch (TranslateException e) {
+            log.error e.message
+            return null
+        }
     }
 }
