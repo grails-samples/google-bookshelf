e42f9902f7ff1640d574ef72867731a6a388e72d
sdelamo <sergio.delamo@softamo.com>
2017-04-25 07:54:32 +0200

    Add log statement

--- app/src/main/groovy/com/example/getstarted/domain/BookDescriptionListener.groovy
+++ app/src/main/groovy/com/example/getstarted/domain/BookDescriptionListener.groovy
@@ -2,6 +2,7 @@ package com.example.getstarted.domain
 
 import grails.config.Config
 import grails.core.support.GrailsConfigurationAware
+import groovy.util.logging.Slf4j
 import org.grails.plugins.googlecloud.translate.GoogleCloudTranslateService
 import org.grails.datastore.mapping.core.Datastore
 import org.grails.datastore.mapping.engine.event.AbstractPersistenceEvent
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationEvent
 import groovy.transform.CompileStatic
 
+@Slf4j
 @CompileStatic
 class BookDescriptionListener extends AbstractPersistenceEventListener implements GrailsConfigurationAware {
 
@@ -34,6 +36,7 @@ class BookDescriptionListener extends AbstractPersistenceEventListener implement
                     || (event.eventType == EventType.PreUpdate && book.isDirty('description')))) {
                 def text = googleCloudTranslateService.translateTextFromSourceToTarget(book.description, source, target)
                 if ( text ) {
+                log.info "text: ${book.description} | source: ${source} | target: ${target} | translation: ${text} "
                     event.entityAccess.setProperty('descriptionSpanish', text)
                 }
 
