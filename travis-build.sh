03a173dd9e5817402ebd539ae9ed49ccadc4d74c
sdelamo <sergio.delamo@softamo.com>
2017-04-27 08:11:22 +0200

    comment publishing

--- travis-build.sh
+++ travis-build.sh
@@ -9,33 +9,33 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
 
     ./gradlew docs:asciidoc || EXIT_STATUS=$?
 
-    if [[ $EXIT_STATUS -eq 0 ]]; then 
+    # if [[ $EXIT_STATUS -eq 0 ]]; then
 
-        if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    
-            if [[ $TRAVIS_TAG == doc_*  ]]; then
-
-                echo "Publishing Documentation"
-                git config --global user.name "$GIT_NAME"
-                git config --global user.email "$GIT_EMAIL"
-                git config --global credential.helper "store --file=~/.git-credentials"
-                echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
-                git clone https://${GH_TOKEN}@github.com/grails-samples/google-bookshelf.git -b gh-pages gh-pages --single-branch > /dev/null
-    
-                cp -r build/docs/build/asciidoc/html5/* gh-pages/
-
-                cd gh-pages
-
-                git add .   
-
-                git commit -a -m "Updating Documentation for Travis Build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-                git push origin HEAD
-                cd ..
-                rm -rf gh-pages
-            fi
-        fi    
-    fi    
+#        if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+#
+#            if [[ $TRAVIS_TAG == doc_*  ]]; then
+#
+#                echo "Publishing Documentation"
+#                git config --global user.name "$GIT_NAME"
+#                git config --global user.email "$GIT_EMAIL"
+#                git config --global credential.helper "store --file=~/.git-credentials"
+#                echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+#
+#                git clone https://${GH_TOKEN}@github.com/grails-samples/google-bookshelf.git -b gh-pages gh-pages --single-branch > /dev/null
+#
+#                cp -r build/docs/build/asciidoc/html5/* gh-pages/
+#
+#                cd gh-pages
+#
+#                git add .
+#
+#                git commit -a -m "Updating Documentation for Travis Build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+#                git push origin HEAD
+#                cd ..
+#                rm -rf gh-pages
+#            fi
+#        fi
+    #  fi
 fi    
 
 exit $EXIT_STATUS
