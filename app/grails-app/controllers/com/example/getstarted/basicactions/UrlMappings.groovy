fa571b309863f5a4e88b6d8d5b66ee29d45fabe4
sdelamo <sergio.delamo@softamo.com>
2017-04-27 19:21:23 +0200

    Improves docs

--- app/grails-app/controllers/com/example/getstarted/basicactions/UrlMappings.groovy
+++ app/grails-app/controllers/com/example/getstarted/basicactions/UrlMappings.groovy
@@ -40,35 +40,14 @@ class UrlMappings {
         // end::updateMap[]
 
         // tag::ahHealth[]
-        '/_ah/health'(controller: 'health') // App Engine flexible environment sends a request to
-                                            // the path '/_ah/health' periodically to check if an
-                                            // instance is still serving requests.
-                                            //
-                                            // For Google Compute Engine, we configure the load
-                                            // balancer health checker to use this path.
-
-        '/_ah/start'(controller: 'health')  // App Engine flexible environment sends a request to the
-                                            // path '/_ah/start' when an instance starts up. This can
-                                            // be useful for warming up resources like in-memory
-                                            // caches and database connection pools.
-                                            //
-                                            // To acheive a similar result with Google Compute Engine,
-                                            // a startup script
-                                            //     https://g.co/cloud/compute/docs/startupscript
-                                            // or an init() method on a servlet with loadOnStartup=1
-                                            // can be used. See ListBookServlet for an example of
-                                            // this.
-
-        '/_ah/stop'(controller: 'health')  // App Engine flexible environment sends a request to the
-                                           // path '/_ah/stop' when an instance is about to be shut
-                                           // down.  This can be useful for cleaning up resources and
-                                           // finishing data transactions.
-                                           //
-                                           // To acheive a similar result with Google Compute Engine,
-                                           // a shutdown script
-                                           // https://g.co/cloud/compute/docs/shutdownscript
-                                           // can be used.
+        '/_ah/health'(controller: 'health')
         // end::ahHealth[]
+        // tag::ahStart[]
+        '/_ah/start'(controller: 'health')
+        // end::ahStart[]
+        // tag::ahStop[]
+        '/_ah/stop'(controller: 'health')
+        // end::ahStop[]
 
         '500'(view: '/error')
         '404'(view: '/notFound')
